954411c6775e839bbb3f801ed076ae7ff6ac74d3
Azamat Kurbanov <azik1kurbanov@gmail.com>
2021-01-21 21:28:17 +0100

    commit

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -1,10 +1,19 @@
 defmodule IslandsEngine.Island do
   alias IslandsEngine.{Coordinate, Island}
 
+  # require IEx; IEx.pry
+
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
-  def new, do: %Island{coordinates: MapSet.new, hit_coordinates: MapSet.new}
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type),
+      %MapSet{} = coordinates <- add_coordnates(offsets, upper_left) do
+        {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new}}
+      else
+        error -> error
+      end
+  end
 
   defp offsets(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
   defp offsets(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
@@ -22,7 +31,26 @@ defmodule IslandsEngine.Island do
   defp add_coordinate(coordinates, %Coordinate{row: row, col: col}, {row_offset, col_offset}) do
     case Coordinate.new(row + row_offset, col + col_offset) do
       {:ok, coordinate} -> {:cont, MapSet.put(coordinates, coordinate)}
-      {:halt, {:error, :invalid_coordinate}}
+      {:error, :invalid_coordinate} -> {:halt, {:error, :invalid_coordinate}}
     end
   end
+
+  def overlaps?(%Island{} = existing_island, %Island{} = new_island) do
+    not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
+  end
+
+  def guess(%Island{} = island, %Coordinate{} = coordinate) do
+    case MapSet.member?(island.coordinates, coordinate) do
+      true ->
+        hit_coordinates = MapSet.put(island.hit_coordinates, coordinate)
+        {:hit, %{island | hit_coordinates: hit_coordinates}}
+      false -> :miss
+    end
+  end
+
+  def forested?(%Island{} = island) do
+    MapSet.equal?(island.coordinates, island.hit_coordinates)
+  end
+
+  def types, do: [:atoll, :dot, :square, :l_shape, :s_shape]
 end
