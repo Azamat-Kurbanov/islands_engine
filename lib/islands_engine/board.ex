d828b7caaac550644a2c7bf6713eed191d88aa90
Azamat Kurbanov <azik1kurbanov@gmail.com>
2021-01-23 16:19:15 +0100

    commit

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -42,4 +42,28 @@ defmodule IslandsEngine.Board do
   defp guess_response(:miss, board) do
     {:miss, :none, :no_win, board}
   end
+
+  defp forest_check(board, key) do
+    case forested?(board, key) do
+      true -> key
+      false -> :none
+    end
+  end
+
+  defp forested?(board, key) do
+    board
+    |> Map.fetch!(key)
+    |> Island.forested?()
+  end
+
+  defp win_check(board) do
+    case all_forested?(board) do
+      true -> :win
+      false -> :no_win
+    end
+  end
+
+  defp all_forested?(board) do
+    Enum.all?(board, fn {_key, island} -> Island.forested?(island) end)
+  end
 end
